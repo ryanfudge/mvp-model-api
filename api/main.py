d1abfd4f6055ed28c0db8b8089823806d8902d4e
Ryan Fudge <ryandfudge@gmail.com>
2024-07-16 22:10:21 -0400

    model trained and dumped into joblib

--- api/main.py
+++ api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from api.models.mvp_model import load_model, predict
-from api.schemas.player_stats import PlayerStats
+from pydantic import BaseModel
+from typing import List
+import numpy as np
 
-app = FastAPI
+from .models.mvp_model import mvp_model
+from .schemas.player_stats import PlayerStats
 
-model=load_model()
+app = FastAPI()
 
+@app.post("/predict")
+def predict_mvp(player_stats: PlayerStats):
+    data = np.array([[player_stats.vorp, player_stats.per, player_stats.ws, 
+                      player_stats.ows, player_stats.dws, player_stats.pts_per_g, 
+                      player_stats.fg_per_g]])
+    prediction = mvp_model.predict(data)
+    return {"mvp_award_share_prediction": prediction[0]}
--- api/models/mvp_model.py
+++ api/models/mvp_model.py
@@ -1,16 +1,8 @@
 import joblib
+import os
 
-def load_model():
-    return joblib.load("models/nba_mvp_model.joblib")
+# Path to the model file
+model_path = os.path.join(os.path.dirname(__file__), '../../models/nba_mvp_model.joblib')
 
-def predict(model, player_stats):
-    input_data = [[
-        player_stats.vorp,
-        player_stats.per,       
-        player_stats.ws,
-        player_stats.ows,
-        player_stats.dws,
-        player_stats.pts_per_g,
-        player_stats.fg_per_g
-    ]]
-    return model.predict(input_data)[0]
+# Load the model
+mvp_model = joblib.load(model_path)
