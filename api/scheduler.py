0b2b2868f9c9a8928fd34aba0355b80a255a68a6
Ryan Fudge <ryandfudge@gmail.com>
2024-07-28 15:39:05 -0400

    Added scheduling

---
+++ api/scheduler.py
@@ -0,0 +1,24 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from datetime import datetime
+import pytz
+
+from scripts.fetch_player_data import fetchToModel 
+
+# Function to fetch player data and run the model
+def scheduled_job():
+    print(f"Fetching player data at {datetime.now()}")
+    # Example player names; modify this as needed
+    player_names = ['Stephen Curry', 'Nikola Jokic', 'Victor Wembanyama', 'Shai Gilgeous-Alexander',
+                    'Joel Embiid', 'Luka Doncic', 'Giannis Antetokounmpo', 'Jayson Tatum', 'Domantas Sabonis',
+                    'Devin Booker', 'Jalen Brunson', 'Anthony Davis']
+    for name in player_names:
+        stats = fetchToModel(name)
+        statslist = dict(name, stats)
+    return statslist
+
+def start_scheduler():
+    scheduler = BackgroundScheduler(timezone=pytz.timezone('US/Eastern'))
+    trigger = CronTrigger(hour=2, minute=0)  # Schedule to run every day at 2 AM ET
+    scheduler.add_job(scheduled_job, trigger)
+    scheduler.start()
